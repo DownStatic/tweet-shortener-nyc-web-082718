da1f9295d1ec5d8e2c42b1ceca58fe275e6100a1
Christopher Stull <chris.stull@gmail.com>
2018-08-16 17:42:30 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -8,7 +8,7 @@ def word_substituter(tweet)
   dictionary = {"hello":"hi","to":"2","two":"2","too":"2","for":"4","four":"4","be":"b","you":"u","at":"@","and":"&"}
   expanded_tweet = tweet.split
   expanded_tweet.collect do |word|
-    if dictionary.keys.include?(word)
+    if dictionary.keys.to_s.include?(word)
       word = dictionary[word]
     end
   end
