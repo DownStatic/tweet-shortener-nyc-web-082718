4a3c325440640a1f5ee4128f93c495709f9398a6
Christopher Stull <chris.stull@gmail.com>
2018-08-16 17:40:38 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,16 @@
 # Write your code here.
+def dictionary
+  dictionary = {"hello":"hi","to":"2","two":"2","too":"2","for":"4","four":"4","be":"b","you":"u","at":"@","and":"&"}
+  return dictionary
+end
+
+def word_substituter(tweet)
+  dictionary = {"hello":"hi","to":"2","two":"2","too":"2","for":"4","four":"4","be":"b","you":"u","at":"@","and":"&"}
+  expanded_tweet = tweet.split
+  expanded_tweet.collect do |word|
+    if dictionary.keys.include?(word)
+      word = dictionary[word]
+    end
+  end
+  return expanded_tweet.join(" ")
+end
