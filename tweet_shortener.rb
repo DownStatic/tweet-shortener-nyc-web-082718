3369ffbce114d8429436671d841b3420e162e44b
Christopher Stull <chris.stull@gmail.com>
2018-08-16 17:51:48 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -8,6 +8,7 @@ def word_substituter(tweet)
   dictionary = {"hello" => "hi","to" =>"2","two" =>"2","too" =>"2","for" =>"4","four" =>"4","be"=>"b","you"=>"u","at"=>"@","and"=>"&"}
   comparison = dictionary.keys
   expanded_tweet = tweet.split
+  new_tweet = []
   expanded_tweet.collect do |word|
     if comparison.include?(word)
       word = dictionary[word]
